929109ad8f50a856b79e3ff98dc0b4b886a07b02
Abhishek Parmar <aparmar@isystango.com>
2018-07-06 10:21:22 +0530

    These are the files for server

---
+++ server/calculatorserver/app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var app = express();
+app.get('/calculator/add',function(req,res){
+    var num1 = Number(req.query.num1);
+    var num2 = Number(req.query.num2);
+    var result;
+    try
+    {
+        result = num1+num2;
+    }
+    catch(error)
+    {
+        result = error.message;
+    }
+    res.send({result: result});
+});
+app.get('/calculator/substract',function(req,res){
+    var num1 =  Number(req.query.num1);
+    var num2 =  Number(req.query.num2);
+    var result;
+    try
+    {
+        result = num1-num2;
+        result = result.toFixed(3);
+    }
+    catch(error)
+    {
+        result = error.message;
+    }
+    res.send({result: result});
+});
+
+app.get('/calculator/multiply',function(req,res){
+    var num1 =  Number(req.query.num1);
+    var num2 =  Number(req.query.num2);
+    var result;
+    try
+    {
+        result = num1*num2;
+        result = result.toFixed(3);
+    }
+    catch(error)
+    {
+        result = error.message;
+    }
+    res.send({result: result});
+});
+
+app.get('/calculator/divide',function(req,res){
+    var num1 =  Number(req.query.num1);
+    var num2 =  Number(req.query.num2);
+    var result;
+    try
+    {
+        if(num2==0) {
+            var errorObject = new Error();
+            errorObject.message = "Number can not be divisible by zero";
+            errorObject.name = "ArithmeticError";
+            throw errorObject;
+        }
+        result = num1/num2;
+        result  = result.toFixed(3);
+    }
+    catch(error)
+    {
+        result = error.message;
+    }
+    res.send({result: result});
+});
+app.listen(3000,()=>console.log("Express Server listening on port Number : 3000"));
